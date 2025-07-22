356f8af14e64a76bdb92d96748f1fdf96051d3a6
RaminSafarli <ramin.seferli.kb@gmail.com>
2025-07-22 19:09:20 +0100

    unrelated_to_this

--- server-backend/app/pipeline/preprocessor.py
+++ server-backend/app/pipeline/preprocessor.py
@@ -1,13 +1,18 @@
 # from bs4 import BeautifulSoup, Comment
 import spacy
-import trafilatura
+from trafilatura import extract, fetch_url
 
 nlp = spacy.load("en_core_web_trf")
 
-def extract_policy_text_trafilatura(html: str) -> str:
-    return trafilatura.extract(html)
+def extract_policy_text_trafilatura(html_path: str) -> str:
+    downloaded_html = fetch_url(html_path)
+    result = extract(downloaded_html)
+    return result
 
 def segment_sentences(text: str) -> list:
+    # print("Segmenting test sentences...")
+    # print(text)
+    # print("Segmented test sentences...")
     """Split text into sentences using spaCy."""
     doc = nlp(text)
     return [sent.text.strip() for sent in doc.sents if sent.text.strip()]
